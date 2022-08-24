39caaf547f37bca4f979499929014bce385ec70e
Mariia <mashulkaegor@mail.ru>
2022-08-24 21:36:33 +0400

    add Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3. Написать программу копирования массива
+Console.WriteLine("Введите длину массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+Random rnd = new Random();
+for (int i = 0; i < n; i++)
+{
+    array[i] = rnd.Next(0, 100);
+}
+Console.WriteLine("Изначальный массив: ");
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+int[] copy = new int [n];
+for (int i = 0; i < n; i++)
+{
+    copy[i] = array[i];
+}
+Console.WriteLine("\nСкопированный массив: ");
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{copy[i]} ");
+}
